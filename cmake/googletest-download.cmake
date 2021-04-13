36ab3e7181e8120ed49e0644e9153624a4ed1fc3
Ingo <ingo@hoeft-online.de>
2021-04-13 12:20:48 +0200

    build: Optimize googletest download
    Check if git is installed, added some minor options to
    ExternalProject_Add(..).

--- cmake/googletest-download.cmake
+++ cmake/googletest-download.cmake
@@ -4,12 +4,14 @@ project(googletest-download NONE)
 
 include(ExternalProject)
 ExternalProject_Add(googletest
-  GIT_REPOSITORY    https://github.com/google/googletest.git
-  GIT_TAG           master
-  SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/googletest-src"
-  BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/googletest-build"
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND     ""
-  INSTALL_COMMAND   ""
-  TEST_COMMAND      ""
+    GIT_REPOSITORY    https://github.com/google/googletest.git
+    GIT_TAG           origin/master
+    GIT_SHALLOW       ON
+    GIT_CONFIG        advice.detachedHead=false
+    SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/googletest-src"
+    BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/googletest-build"
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND     ""
+    INSTALL_COMMAND   ""
+    TEST_COMMAND      ""
 )
