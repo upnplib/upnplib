94e90ac723486367e735863fa1b65436103cdfcd
Ingo Höft <Ingo@Hoeft-online.de>
2021-10-03 01:02:51 +0200

    test: Merge two UpnpString gtests

--- gtests/test_UpnpString.cpp
+++ gtests/test_UpnpString.cpp
@@ -1,24 +1,158 @@
 // Copyright (C) 2021 GPL 3 and higher by Ingo Höft,  <Ingo@Hoeft-online.de>
 // Redistribution only with this Copyright remark. Last modified: 2021-10-02
 
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 #undef HAVE_STRNLEN
 #undef HAVE_STRNDUP
 
+using ::testing::_;
+using ::testing::Eq;
+using ::testing::Return;
+
+// --- mock calloc ---------------------------------------
+class CMock_calloc {
+  public:
+    MOCK_METHOD(void*, calloc, (size_t nmemb, size_t size));
+};
+CMock_calloc* ptrMock_calloc = nullptr;
+
+// --- mock free -----------------------------------------
+class CMock_free {
+  public:
+    MOCK_METHOD(void, free, (void* ptr));
+};
+CMock_free* ptrMock_free = nullptr;
+
+
 namespace { // no name, i.e. anonymous for file scope
             // this is the C++ way for decorator STATIC
 
-void free(void* ptr) {
-    // This function overwrites the 'free' function from the system library.
-    // This is only possible with a local (file) scope of this function. Also it
-    // must be defined before the included program. It frees allocated memory
-    // from the heap. The test settings does not use memory on the heap so this
-    // dummy function just do nothing.
+// The following functions overwrite the functions from the system library.
+// This is only possible with a local (file) scope of these functions. Also they
+// must be defined before the included program.
+
+void* calloc(size_t nmemb, size_t size) {
+    return ptrMock_calloc->calloc(nmemb, size);
+}
+void free(void* ptr) { return ptrMock_free->free(ptr);
 }
 
 #include "api/UpnpString.cpp"
+
 } // namespace
+
+
+// testsuite with fixtures
+//------------------------
+class UpnpStringMockTestSuite : public ::testing::Test {
+  protected:
+    // Instantiate the mock objects.
+    // The global pointer to them are set in the constructor below.
+    CMock_calloc mock_calloc;
+    CMock_free mock_free;
+
+    UpnpStringMockTestSuite() {
+        // set the global pointer to the mock objects
+        ptrMock_calloc = &mock_calloc;
+        ptrMock_free = &mock_free;
+    }
+};
+
+TEST_F(UpnpStringMockTestSuite, createNewUpnpString) {
+    // provide a structure of a UpnpString
+    char mstring[] = {0};
+    SUpnpString upnpstr = {};
+    UpnpString* p = (UpnpString*)&upnpstr;
+    UpnpString* str;
+
+    EXPECT_CALL(mock_calloc, calloc(_, _))
+        .WillOnce(Return(p))
+        .WillOnce(Return(*&mstring));
+    // call the unit
+    str = UpnpString_new();
+    EXPECT_THAT(str, Eq(p));
+
+    // test edge conditions
+    EXPECT_CALL(mock_calloc, calloc(_, _)).WillOnce(Return((UpnpString*)NULL));
+    // call the unit
+    str = UpnpString_new();
+    EXPECT_EQ(str, (UpnpString*)NULL);
+
+    EXPECT_CALL(mock_calloc, calloc(_, _))
+        .WillOnce(Return(p))
+        .WillOnce(Return((char*)NULL));
+    EXPECT_CALL(mock_free, free(_)).Times(1);
+    // call the unit
+    str = UpnpString_new();
+    EXPECT_EQ(str, (UpnpString*)NULL);
+}
+
+TEST_F(UpnpStringMockTestSuite, deleteUpnpString) {
+    // provide a UpnpString
+    char mstring[] = "hello world";
+    SUpnpString upnpstr = {11, *&mstring};
+    UpnpString* p = (UpnpString*)&upnpstr;
+
+    // Check edge condition
+    UpnpString_delete((UpnpString*)nullptr);
+    EXPECT_EQ(upnpstr.m_length, 11);
+    EXPECT_STREQ(upnpstr.m_string, "hello world");
+
+    // call the unit
+    EXPECT_CALL(mock_free, free(_)).Times(2);
+    UpnpString_delete(p);
+    EXPECT_EQ(upnpstr.m_length, 0);
+    EXPECT_EQ(upnpstr.m_string, (char*)NULL);
+}
+
+TEST_F(UpnpStringMockTestSuite, setUpnpString) {
+    // provide an empty UpnpString
+    char mstring[] = {0};
+    SUpnpString upnpstr = {0, *&mstring};
+    UpnpString* p = (UpnpString*)&upnpstr;
+
+    // call the unit
+    EXPECT_CALL(mock_free, free(_)).Times(1);
+    EXPECT_PRED2(UpnpString_set_String, p, (const char*)"set string");
+    EXPECT_EQ(upnpstr.m_length, 10);
+    EXPECT_STREQ(upnpstr.m_string, "set string");
+}
+
+TEST_F(UpnpStringMockTestSuite, setUpnpStringN) {
+    // provide an empty UpnpString
+    char mstring[] = {0};
+    SUpnpString upnpstr = {0, *&mstring};
+    UpnpString* p = (UpnpString*)&upnpstr;
+
+    // call the unit
+    EXPECT_CALL(mock_free, free(_)).Times(1);
+    EXPECT_PRED3(UpnpString_set_StringN, p, (const char*)"hello world", 0);
+    EXPECT_EQ(upnpstr.m_length, 0);
+    EXPECT_STREQ(upnpstr.m_string, "");
+
+    EXPECT_CALL(mock_free, free(_)).Times(1);
+    EXPECT_PRED3(UpnpString_set_StringN, p, (const char*)"hello world", 1);
+    EXPECT_EQ(upnpstr.m_length, 1);
+    EXPECT_STREQ(upnpstr.m_string, "h");
+
+    EXPECT_CALL(mock_free, free(_)).Times(1);
+    EXPECT_PRED3(UpnpString_set_StringN, p, (const char*)"hello world", 10);
+    EXPECT_EQ(upnpstr.m_length, 10);
+    EXPECT_STREQ(upnpstr.m_string, "hello worl");
+
+    EXPECT_CALL(mock_free, free(_)).Times(1);
+    EXPECT_PRED3(UpnpString_set_StringN, p, (const char*)"hello world", 11);
+    EXPECT_EQ(upnpstr.m_length, 11);
+    EXPECT_STREQ(upnpstr.m_string, "hello world");
+
+    EXPECT_CALL(mock_free, free(_)).Times(1);
+    EXPECT_PRED3(UpnpString_set_StringN, p, (const char*)"hello world", 12);
+    EXPECT_EQ(upnpstr.m_length, 11);
+    EXPECT_STREQ(upnpstr.m_string, "hello world");
+}
+
 
 // simple testsuite without fixtures
 //----------------------------------
@@ -65,63 +199,6 @@ TEST(UpnpStringTestSuite, strndupCalledWithVariousParameter) {
 #ifndef OLD_TEST
     ADD_FAILURE() << "# strndup isn't used in the UPnPlib core.";
 #endif
-}
-
-TEST(UpnpStringTestSuite, deleteUpnpString) {
-    // provide a UpnpString
-    char mstring[] = "hello world";
-    SUpnpString upnpstr = {11, *&mstring};
-    UpnpString* p = (UpnpString*)&upnpstr;
-
-    // Check edge condition
-    UpnpString_delete((UpnpString*)nullptr);
-    EXPECT_EQ(upnpstr.m_length, 11);
-    EXPECT_STREQ(upnpstr.m_string, "hello world");
-
-    // call the unit
-    UpnpString_delete(p);
-    EXPECT_EQ(upnpstr.m_length, 0);
-    EXPECT_EQ(upnpstr.m_string, (char*)NULL);
-}
-
-TEST(UpnpStringTestSuite, setUpnpString) {
-    // provide an empty UpnpString
-    char mstring[] = {0};
-    SUpnpString upnpstr = {0, *&mstring};
-    UpnpString* p = (UpnpString*)&upnpstr;
-
-    // call the unit
-    EXPECT_PRED2(UpnpString_set_String, p, (const char*)"set string");
-    EXPECT_EQ(upnpstr.m_length, 10);
-    EXPECT_STREQ(upnpstr.m_string, "set string");
-}
-
-TEST(UpnpStringTestSuite, setUpnpStringN) {
-    // provide an empty UpnpString
-    char mstring[] = {0};
-    SUpnpString upnpstr = {0, *&mstring};
-    UpnpString* p = (UpnpString*)&upnpstr;
-
-    // call the unit
-    EXPECT_PRED3(UpnpString_set_StringN, p, (const char*)"hello world", 0);
-    EXPECT_EQ(upnpstr.m_length, 0);
-    EXPECT_STREQ(upnpstr.m_string, "");
-
-    EXPECT_PRED3(UpnpString_set_StringN, p, (const char*)"hello world", 1);
-    EXPECT_EQ(upnpstr.m_length, 1);
-    EXPECT_STREQ(upnpstr.m_string, "h");
-
-    EXPECT_PRED3(UpnpString_set_StringN, p, (const char*)"hello world", 10);
-    EXPECT_EQ(upnpstr.m_length, 10);
-    EXPECT_STREQ(upnpstr.m_string, "hello worl");
-
-    EXPECT_PRED3(UpnpString_set_StringN, p, (const char*)"hello world", 11);
-    EXPECT_EQ(upnpstr.m_length, 11);
-    EXPECT_STREQ(upnpstr.m_string, "hello world");
-
-    EXPECT_PRED3(UpnpString_set_StringN, p, (const char*)"hello world", 12);
-    EXPECT_EQ(upnpstr.m_length, 11);
-    EXPECT_STREQ(upnpstr.m_string, "hello world");
 }
 
 TEST(UpnpStringTestSuite, clearUpnpString) {
