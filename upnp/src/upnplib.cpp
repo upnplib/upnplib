9a047b921becd4adb028307bfac1a95abc8e78f9
Ingo <ingo@hoeft-online.de>
2021-10-21 22:41:49 +0200

    test: Improve upnplib executable info program
    Add some tests to check compiling and linking in the real production
    environment and also to check if the real system calls are correct
    addressed because they are wrapped for mocking.

--- upnp/src/upnplib.cpp
+++ upnp/src/upnplib.cpp
@@ -28,86 +28,243 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * Copyright (C) 2021 GPL 3 and higher by Ingo Höft,  <Ingo@Hoeft-online.de>
- * Redistribution only with this Copyright remark. Last modified: 2021-10-10
+ * Redistribution only with this Copyright remark. Last modified: 2021-10-21
  *
  *************************************************************************/
 
+#include "FreeList.h"
 #include "pthread.h"
 #include "upnpconfig.h"
+#include "upnpdebug.h"
 
 #include <iostream>
+#include <sstream>
 
 #include "port.hpp"
 
 namespace upnp {
 
-void* start_routine(void*) {
-    std::cout << "UPNP_VERSION_STRING = " << UPNP_VERSION_STRING << "\n";
-    std::cout << "UPNP_VERSION_MAJOR = " << UPNP_VERSION_MAJOR << "\n";
-    std::cout << "UPNP_VERSION_MINOR = " << UPNP_VERSION_MINOR << "\n";
-    std::cout << "UPNP_VERSION_PATCH = " << UPNP_VERSION_PATCH << "\n";
-    std::cout << "UPNP_VERSION = " << UPNP_VERSION << "\n";
+void* library_info(void*) {
+    // Using this will not split output string on '<<' by output from other
+    // threads.
+    std::stringstream msg;
 
+    msg << "---- library info ----------------\n"
+        << "UPNP_VERSION_STRING = " << UPNP_VERSION_STRING << "\n"
+        << "UPNP_VERSION_MAJOR = " << UPNP_VERSION_MAJOR << "\n"
+        << "UPNP_VERSION_MINOR = " << UPNP_VERSION_MINOR << "\n"
+        << "UPNP_VERSION_PATCH = " << UPNP_VERSION_PATCH << "\n"
+        << "UPNP_VERSION = " << UPNP_VERSION << "\n";
 /*
  * Check library optional features
  */
 #if UPNP_HAVE_DEBUG
-    std::cout << "UPNP_HAVE_DEBUG \t= yes\n";
+    msg << "UPNP_HAVE_DEBUG \t= yes\n";
 #else
-    std::cout << "UPNP_HAVE_DEBUG \t= no\n";
+    msg << "UPNP_HAVE_DEBUG \t= no\n";
 #endif
 
 #if UPNP_HAVE_CLIENT
-    std::cout << "UPNP_HAVE_CLIENT \t= yes\n";
+    msg << "UPNP_HAVE_CLIENT \t= yes\n";
 #else
-    std::cout << "UPNP_HAVE_CLIENT \t= no\n";
+    msg << "UPNP_HAVE_CLIENT \t= no\n";
 #endif
 
 #if UPNP_HAVE_DEVICE
-    std::cout << "UPNP_HAVE_DEVICE \t= yes\n";
+    msg << "UPNP_HAVE_DEVICE \t= yes\n";
 #else
-    std::cout << "UPNP_HAVE_DEVICE \t= no\n";
+    msg << "UPNP_HAVE_DEVICE \t= no\n";
 #endif
 
 #if UPNP_HAVE_WEBSERVER
-    std::cout << "UPNP_HAVE_WEBSERVER \t= yes\n";
+    msg << "UPNP_HAVE_WEBSERVER \t= yes\n";
 #else
-    std::cout << "UPNP_HAVE_WEBSERVER \t= no\n";
+    msg << "UPNP_HAVE_WEBSERVER \t= no\n";
 #endif
 
 #if UPNP_HAVE_TOOLS
-    std::cout << "UPNP_HAVE_TOOLS \t= yes\n";
+    msg << "UPNP_HAVE_TOOLS \t= yes\n";
 #else
-    std::cout << "UPNP_HAVE_TOOLS \t= no\n";
+    msg << "UPNP_HAVE_TOOLS \t= no\n";
 #endif
 
-    std::cout << "-- POSIX Thread ended" << std::endl;
-    return (0); // calls pthread_exit()
+    msg << "----------------------------------\n";
+    std::cout << msg.str();
+
+    return (void*)0; // calls pthread_exit()
 }
+
+// Simple check of module FreeList
+// -------------------------------
+#ifdef _WIN32
+// There is a problem with EXPORT_SPEC. We have to modify the original source
+// upnp/src/threadutil/FreeList.h to export its function names. Modifications
+// of the original sources are not intended at this stage of re-engeneering.
+void* check_freelist(void*) {
+    std::cout << "-> Check module FreeList is temporary disabled on MS Windows "
+                 "due to re-engeneering issues.\n";
+    return (void*)0;
+}
+#else
+// Running in the production environment here. For extended testing look at
+// the Unit Tests.
+void* check_freelist(void*) {
+    FreeList free_list{};
+    // Init with chunk size of 1 byte and max. 3 entries in the freelist.
+    ::FreeListInit(&free_list, 1, 3);
+
+    // Get new nodes. Because the freelist is empty, they should be allocated
+    // from memory.
+    FreeListNode* newnode1 = (FreeListNode*)::FreeListAlloc(&free_list);
+    FreeListNode* newnode2 = (FreeListNode*)::FreeListAlloc(&free_list);
+    FreeListNode* newnode3 = (FreeListNode*)::FreeListAlloc(&free_list);
+    FreeListNode* newnode4 = (FreeListNode*)::FreeListAlloc(&free_list);
+
+    // Put unused nodes to the freelist.
+    ::FreeListFree(&free_list, newnode1);
+    ::FreeListFree(&free_list, newnode2);
+    ::FreeListFree(&free_list, newnode3);
+    ::FreeListFree(&free_list, newnode4);
+
+    // Get them again. They should now mostly come from the freelist without
+    // expensive allocation from memory.
+    newnode1 = (FreeListNode*)::FreeListAlloc(&free_list);
+    newnode2 = (FreeListNode*)::FreeListAlloc(&free_list);
+    newnode3 = (FreeListNode*)::FreeListAlloc(&free_list);
+    newnode4 = (FreeListNode*)::FreeListAlloc(&free_list);
+
+    // Destray the freelist. This should free all allocated memory for the
+    // nodes.
+    ::FreeListDestroy(&free_list);
+
+    return (void*)0;
+}
+#endif // _WIN32
+
+//
+// Simple check of module upnpdebug
+// --------------------------------
+#ifdef _WIN32
+// There is a problem with EXPORT_SPEC. We have to modify the original source
+// upnp/inc/upnpdebug.h to export its function names. Modifications
+// of the original sources are not intended at this stage of re-engeneering.
+void* check_upnpdebug(void*) {
+    std::cout
+        << "-> Check module upnpdebug is temporary disabled on MS Windows "
+           "due to re-engeneering issues.\n";
+    return (void*)0;
+}
+#else // _WIN32
+
+// Running in the production environment here. For extended testing look at
+// the Unit Tests.
+void* check_upnpdebug(void*) {
+#ifdef NDEBUG
+    std::cout << "-> Module upnpdebug is disabled because NDEBUG is set.\n";
+#else // NDEBUG
+    // This must be set before initialization
+    ::UpnpSetLogLevel(UPNP_ALL);
+
+    int rc = ::UpnpInitLog();
+    if (rc != UPNP_E_SUCCESS) {
+        std::cerr << "Error: UpnpInitLog() failed with return code = " << rc
+                  << std::endl;
+        return (void*)1;
+    }
+    FILE* fd = ::UpnpGetDebugFile((Upnp_LogLevel)NULL, (Dbg_Module)NULL);
+    if (fd != stderr) {
+        std::cerr << "Error! UpnpGetDebugFile(..) does not return \'stderr\' "
+                     "for output.\n";
+        return (void*)2;
+    }
+    ::UpnpPrintf(UPNP_ALL, API, __FILE__, __LINE__, "Simple check for %s.\n",
+                 "UpnpPrintf");
+    ::UpnpCloseLog();
+
+#endif // NDEBUG
+    return (void*)0;
+}
+#endif // _WIN32
+
 } // namespace upnp
 
+//
+// main entry
+// ----------
 int main() {
-    pthread_t thread;
     int rc;
     void* retval;
 
-    std::cout << "-- starting POSIX Thread" << std::endl;
+    // Starting POSIX Threads
+    // ----------------------
+    std::cout << "-- Starting POSIX Threads ..." << std::endl;
 
-    rc = pthread_create(&thread, NULL, &upnp::start_routine, NULL);
+    pthread_t thread_info;
+
+    rc = pthread_create(&thread_info, NULL, &upnp::library_info, NULL);
     if (rc != 0) {
-        std::cerr << "Error! unable to create thread, " << rc << std::endl;
+        std::cerr << "Error! unable to create thread_info, " << rc << std::endl;
         exit(1);
     }
 
-    rc = pthread_join(thread, &retval);
+    pthread_t thread_freelist;
+
+    rc = pthread_create(&thread_freelist, NULL, &upnp::check_freelist, NULL);
     if (rc != 0) {
-        std::cerr << "Error! Unable to join thread with rc=" << rc << std::endl;
+        std::cerr << "Error! unable to create thread_freelist, " << rc
+                  << std::endl;
+        exit(1);
+    }
+
+    pthread_t thread_upnpdebug;
+
+    rc = pthread_create(&thread_upnpdebug, NULL, &upnp::check_upnpdebug, NULL);
+    if (rc != 0) {
+        std::cerr << "Error! unable to create thread_upnpdebug, " << rc
+                  << std::endl;
+        exit(1);
+    }
+
+    // Waiting for threads to finish
+    // -----------------------------
+    rc = pthread_join(thread_info, &retval);
+    if (rc != 0) {
+        std::cerr << "Error! Unable to join thread_info with rc=" << rc
+                  << std::endl;
         exit(1);
     }
     if (retval != NULL) {
-        std::cerr << "Error! Thread failed with retval=" << retval << std::endl;
+        std::cerr << "Error! Thread_info failed with retval="
+                  << ((int)(intptr_t)retval) << std::endl;
+        exit((int)(intptr_t)retval);
+    }
+    std::cout << "-- POSIX Thread for library info done." << std::endl;
+
+    rc = pthread_join(thread_freelist, &retval);
+    if (rc != 0) {
+        std::cerr << "Error! Unable to join thread_freelist with rc=" << rc
+                  << std::endl;
         exit(1);
     }
+    if (retval != NULL) {
+        std::cerr << "Error! Thread_info failed with retval="
+                  << ((int)(intptr_t)retval) << std::endl;
+        exit((int)(intptr_t)retval);
+    }
+    std::cout << "-- POSIX Thread to check module FreeList done." << std::endl;
 
-    return (0);
+    rc = pthread_join(thread_upnpdebug, &retval);
+    if (rc != 0) {
+        std::cerr << "Error! Unable to join thread_upnpdebug with rc=" << rc
+                  << std::endl;
+        exit(1);
+    }
+    if (retval != NULL) {
+        std::cerr << "Error! Thread_upnpdebug failed with retval="
+                  << ((int)(intptr_t)retval) << std::endl;
+        exit((int)(intptr_t)retval);
+    }
+    std::cout << "-- POSIX Thread to check module upnpdebug done." << std::endl;
+
+    return 0;
 }
