913bb27a547bed6c4407bf0926b38ca4f3d31366
Ingo <ingo@hoeft-online.de>
2022-05-19 16:44:27 +0200

    test: Rework gtests to run with internal libraries
    Rework 'test_template', improve 'test_simple' to test for correct
    linking and propagating (PUBLIC, INTERFACE, PRIVATE) of system libraries
    on the internal libraries.

--- gtests/test_simple.cpp
+++ gtests/test_simple.cpp
@@ -1,9 +1,17 @@
 // Copyright (C) 2021 GPL 3 and higher by Ingo Höft,  <Ingo@Hoeft-online.de>
 // Redistribution only with this Copyright remark. Last modified: 2022-05-05
 
+#include "pthread.h" // To find pthreads4w don't use <pthread.h>
 #include "gmock/gmock.h"
 
-#include "pthread.h" // To find pthreads4w don't use <pthread.h>
+#ifdef _WIN32
+#include <winsock2.h>
+#include <iphlpapi.h> // must be after <winsock2.h>
+#else
+#include <sys/socket.h>
+#endif
+
+namespace upnplib {
 
 // simple mocked class
 // -------------------
@@ -18,7 +26,7 @@ class MockFoo : public Foo {
 
 // simple testsuite
 //-----------------
-TEST(simpleTestSuite, simpleMockTest) {
+TEST(simpleTestSuite, simple_mock_test) {
     MockFoo mockedFoo;
 
     EXPECT_CALL(mockedFoo, GetSize()).Times(1);
@@ -27,7 +35,7 @@ TEST(simpleTestSuite, simpleMockTest) {
 
 void* pthread_start_routine(void*) { return nullptr; }
 
-TEST(simpleTestSuite, simplePthreadsTest) {
+TEST(simpleTestSuite, simple_pthreads_test) {
     pthread_t thread;
     void* retval;
 
@@ -35,6 +43,40 @@ TEST(simpleTestSuite, simplePthreadsTest) {
     EXPECT_EQ(pthread_join(thread, &retval), 0);
     EXPECT_EQ(retval, (void*)NULL);
 }
+
+#ifdef _WIN32
+// Test library ws2_32 (winsock2.h)
+TEST(simpleTestSuite, simple_winsock_test) {
+    // Initialize winsocket
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    EXPECT_EQ(iResult, 0);
+
+    // Just create and close a socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    EXPECT_NE(sockfd, -1);
+    EXPECT_EQ(closesocket(sockfd), 0);
+}
+
+// Test library iphlpapi (iphlpapi.h)
+TEST(simpleTestSuite, simple_iphlpapi_test) {
+    // Just ask for a network adapter index
+    wchar_t AdapterName[] = L"not_existing";
+    PULONG IfIndex{0};
+    EXPECT_EQ(GetAdapterIndex(AdapterName, IfIndex), 87);
+}
+#else
+
+// Test unix winsock
+TEST(simpleTestSuite, simple_winsock_test) {
+    // Just create and close a socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    EXPECT_NE(sockfd, -1);
+    EXPECT_EQ(close(sockfd), 0);
+}
+#endif
+
+} // namespace upnplib
 
 // main entry
 // ----------
